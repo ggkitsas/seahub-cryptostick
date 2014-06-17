1f446dfbb324f2f636faf552808662503560130c
ggkitsas <ggkitsas@yahoo.com>
2014-06-17 15:48:26 +0300

    minor changes

--- build.sh
+++ build.sh
@@ -9,28 +9,35 @@ SEAHUB_DIR=$ROOT_DIR/seafile-server/seahub
 BUILD_DIR=$ROOT_DIR/build
 CCNET_CONF_DIR=$BUILD_DIR/.ccnet
 SEAFILE_CONF_DIR=$BUILD_DIR/.seaf-server
+PATCH_DIR=$ROOT_DIR/patches
 
-export PREFIX=$BULD_DIR
+export PREFIX=$BUILD_DIR
 export PATH=$BUILD_DIR/bin:$PATH
 export LD_LIBRARY_PATH=$BUILD_DIR/lib:$LD_LIBRARY_PATH
 export PKG_CONFIG_PATH=$BUILD_DIR/lib/pkgconfig:$PKG_CONFIG_PATH
 
+# First clean everything
+./clean.sh
+
 # Build libsearpc
 cd $SEARPC_DIR
-./autogen
+./autogen.sh
 ./configure --prefix=$PREFIX
+cp $PATCH_DIR/searpc-marshal.h.libsearpc ./demo/searpc-marshal.h
 make -j$1 && make install
 
 # Build ccnet
 cd $CCNET_DIR
-./autogen
+./autogen.sh
 ./configure --prefix=$PREFIX --disable-client --enable-server
+cp $PATCH_DIR/searpc-marshal.h.ccnet ./lib/searpc-marshal.h
 make -j$1 && make install
 
 # Build seafile
 cd $SEAFILE_DIR
-./autogen
+./autogen.sh
 ./configure --prefix=$PREFIX --disable-client --enable-server
+cp $PATCH_DIR/searpc-marshal.h.seafile ./lib/searpc-marshal.h
 make -j$1 && make install
 
 # Deploy Seahub
