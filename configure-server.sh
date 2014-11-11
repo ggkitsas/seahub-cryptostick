20e4dbc181a10ac22be31ca7c645c9acbc80d893
george <ggkitsas@yahoo.com>
2014-11-11 15:18:51 +0100

    python paths

--- configure-server.sh
+++ configure-server.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+ROOT_DIR=`pwd`
+SEAHUB_DIR=$ROOT_DIR/seafile-server/seahub
+export PYTHONPATH=`pwd`/build/lib/python2.7/site-packages:$SEAHUB_DIR/thirdpart
+export PATH=$PATH:`pwd`/build/bin
 build/bin/seafile-admin setup
 
